abe9894c22b9dbc15cd0ffbb2c7179db8f7d4e8e
Zach Snyder <zachsnyder3@gmail.com>
2017-12-15 22:37:00 -0800

    add optarg in main again

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,26 @@
 
 int main(int argc, char *argv[]) {
     Engine *engine = new Engine();
-    WaveTableSynth *synth = new WaveTableSynth();
+    WaveTableSynth *synth;
     ShellController *shell = new ShellController();
+    int continuous = 0;
+    int option;
+
+    // parse command line args
+    while ((option = getopt(argc, argv, "c")) != -1) {
+        switch (option) {
+            case 'c':
+                continuous = 1;
+                break;
+        }
+    }
+    
+    if(continuous) {
+        synth = new WaveTableSynth(1);
+    } else {
+        synth = new WaveTableSynth();
+    }
+
     
     engine->add_instrument(synth);
     engine->add_controller(shell);
