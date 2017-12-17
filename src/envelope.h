93a5c9268acd96148f6c576cfe5fdc3f78ddf046
Zach Snyder <zachsnyder3@gmail.com>
2017-12-16 19:25:24 -0800

    change Envelope defines to constants

--- src/envelope.h
+++ src/envelope.h
@@ -9,26 +9,29 @@
 #ifndef envelope_h
 #define envelope_h
 
-#define DEFAULT_ATTACK (1000)
-#define DEFAULT_DECAY (10000)
-#define DEFAULT_SUSTAIN (1000)
-#define DEFAULT_RELEASE (50000)
-#define DEFAULT_SUSTAIN_LEVEL (0.5)
-
 #include "littledaw.h"
 
+class EnvelopeConstants {
+public:
+    static const int DEFAULT_ATTACK = 1000;
+    static const int DEFAULT_DECAY = 10000;
+    static const int DEFAULT_SUSTAIN = 1000;
+    static const int DEFAULT_RELEASE = 50000;
+    constexpr static const float DEFAULT_SUSTAIN_LEVEL = 0.5;
+};
+
 // Finite Envelope
-class Envelope {
+class Envelope : public EnvelopeConstants{
     // envelope parameters
     int node1, node2, node3;
     float sustain_level;
 public:
     int length; // in samples
-    Envelope(int a=DEFAULT_ATTACK, 
-             int d=DEFAULT_DECAY, 
-             int s=DEFAULT_SUSTAIN, 
-             int r=DEFAULT_RELEASE, 
-             float sustain_level=DEFAULT_SUSTAIN_LEVEL);
+    Envelope(int a=Envelope::DEFAULT_ATTACK, 
+             int d=Envelope::DEFAULT_DECAY, 
+             int s=Envelope::DEFAULT_SUSTAIN, 
+             int r=Envelope::DEFAULT_RELEASE, 
+             float sustain_level=Envelope::DEFAULT_SUSTAIN_LEVEL);
     float calculate(int, bool);
 };
 
